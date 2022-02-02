aee5e613300d816cabbba4e74f0dbfc3655a12f6
otarza <otar.zakalashvili@gmail.com>
2022-02-02 11:37:56 +0400

    #3 - Implement weather service methods.

--- src/Services/WeatherService.php
+++ src/Services/WeatherService.php
@@ -5,18 +5,33 @@ namespace Drupal\monsoon_tech_exam\Services;
 class WeatherService {
 
   public function fetchWeatherData(){
-    //TODO implement fetching weather data.
-    return "Irland Weather";
+    $httpClient = \Drupal::httpClient();
+    $request = $httpClient->request('GET', 'https://api.openweathermap.org/data/2.5/weather?q=Dublin&APPID=169e10f26f321bd987fe9fb03bdaab85');
+
+    return $request->getBody()->getContents();
   }
 
   public function storeWeatherData($weatherDataJSONString){
-    //TODO implement storing weather data.
-    return "Stored";
+    \Drupal::state()->set('monsoon_tech_exam.weather_data', $weatherDataJSONString);
   }
 
   public function loadWeatherData(){
-    //TODO implement loading weather data.
-    return "Weather data loaded.";
+    return \Drupal::state()->get('monsoon_tech_exam.weather_data');
+  }
+
+  public function getWeatherData() {
+    // Try to load stored weather data.
+    $weatherData = $this->loadWeatherData();
+
+
+    // Check if weather data was returned from State storage.
+    if (empty($weatherData)) {
+      // Fetch and store weather data if it is not yet stored.
+      $weatherData = $this->fetchWeatherData();
+      $this->storeWeatherData($weatherData);
+    }
+
+    return $weatherData;
   }
 
 }
