4d37cde2422233749fe2dbe259ac081406048702
otarza <otar.zakalashvili@gmail.com>
2022-02-02 12:55:12 +0400

    #3 - cleanup, fix coding standard issues, add admin configuration menu item.

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Monsoon Tech Exam - Weather
 Fetch weather data and store it using State API
-
--- src/Services/WeatherService.php
+++ src/Services/WeatherService.php
@@ -2,33 +2,62 @@
 
 namespace Drupal\monsoon_tech_exam\Services;
 
+/**
+ * A service providing functionality to communicate with weather API.
+ */
 class WeatherService {
 
-  public function fetchWeatherData(){
-    $httpClient = \Drupal::httpClient();
-    $request = $httpClient->request('GET', 'https://api.openweathermap.org/data/2.5/weather?q=Dublin&APPID=169e10f26f321bd987fe9fb03bdaab85');
+  /**
+   * Fetches weather data from API.
+   */
+  public function fetchWeatherData(): ?string {
+    $contents = NULL;
+    $api_key = \Drupal::state()->get('monsoon_tech_exam.api_key');
+    if (!empty($api_key)) {
+      $city_name = \Drupal::state()->get('monsoon_tech_exam.city_name');
+      $request_options = [
+        'query' => [
+          'q' => ($city_name) ?: "Dublin",
+          'appid' => $api_key,
+        ],
+      ];
+      $httpClient = \Drupal::httpClient();
+      $request = $httpClient->request('GET', 'https://api.openweathermap.org/data/2.5/weather', $request_options);
+      $contents = $request->getBody()->getContents();
+    }
 
-    return $request->getBody()->getContents();
+    return $contents;
   }
 
-  public function storeWeatherData($weatherDataJSONString){
+  /**
+   * Store weather data using State API.
+   */
+  public function storeWeatherData($weatherDataJSONString) {
     \Drupal::state()->set('monsoon_tech_exam.weather_data', $weatherDataJSONString);
   }
 
-  public function loadWeatherData(){
+  /**
+   * Load weather data using State API.
+   */
+  public function loadWeatherData() {
     return \Drupal::state()->get('monsoon_tech_exam.weather_data');
   }
 
+  /**
+   * Get weather data either from State API or weather API.
+   */
   public function getWeatherData() {
-    // Try to load stored weather data.
-    $weatherData = $this->loadWeatherData();
-
-
-    // Check if weather data was returned from State storage.
-    if (empty($weatherData)) {
-      // Fetch and store weather data if it is not yet stored.
-      $weatherData = $this->fetchWeatherData();
-      $this->storeWeatherData($weatherData);
+    $weatherData = NULL;
+    $api_key = \Drupal::state()->get('monsoon_tech_exam.api_key');
+    if (!empty($api_key)) {
+      // Try to load stored weather data.
+      $weatherData = $this->loadWeatherData();
+      // Check if weather data was returned from State storage.
+      if (empty($weatherData)) {
+        // Fetch and store weather data if it is not yet stored.
+        $weatherData = $this->fetchWeatherData();
+        $this->storeWeatherData($weatherData);
+      }
     }
 
     return $weatherData;
